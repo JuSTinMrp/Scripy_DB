282c2c162984acd9c5b0351161c06208db0ba6ff
JuSTinMrp <r4mp3nt3st3r619@gmail.com>
2023-12-03 09:53:44 +0530

    editing metadata

--- main.lua
+++ main.lua
@@ -1,14 +1,15 @@
--- HEAD --
+-- HEAD -- -- Meta data --
 
 description = [[
 This is to be modified later
 ]]
 
 author= "Ram Praveen (cyberrav)"
+categories = {"discovery", "safe"}
+
 
 -- RULE --
 
 
 
 -- ACTION --
-
